084db3ac51f8596f0b054f321f2f5f3719031515
goncalhurtado <gonzalomilano@gmail.com>
2023-11-08 09:14:08 -0300

    added style to machine and screen component

--- src/components/Machine.jsx
+++ src/components/Machine.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Screen from "./Screen.jsx";
 
 const Machine = () => {
-  return <div className="probando">Machine</div>;
+  return (
+    <div className="machine">
+      <div className="head">
+        <h1>Timba</h1>
+      </div>
+      <div className="machine__front">
+        <div className="machine__front__bezel">
+          <Screen />
+        </div>
+      </div>
+      <div className="back">
+        <input className="back__play" type="button" value="Play" />
+        <input className="back__stop" type="button" value="Stop" />
+      </div>
+    </div>
+  );
 };
 
 export default Machine;
---
+++ src/components/Screen.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Screen = () => {
+  return (
+    <div className="screen">
+      <div className="ironTop"></div>
+      <div className="ironBotom"></div>
+      <div className="screen__col">
+        <div className="screen__col__item">b</div>
+        <div className="screen__col__item">b</div>
+        <div className="screen__col__item">b</div>
+      </div>
+      <div className="screen__col">
+        <div className="screen__col__item">b</div>
+        <div className="screen__col__item">b</div>
+        <div className="screen__col__item">b</div>
+      </div>
+      <div className="screen__col">
+        <div className="screen__col__item">b</div>
+        <div className="screen__col__item">b</div>
+        <div className="screen__col__item">b</div>
+      </div>
+    </div>
+  );
+};
+
+export default Screen;
