3f6dbb952e8638f34964002ed6b05e0e7d6e2f66
goncalhurtado <gonzalomilano@gmail.com>
2023-11-08 07:51:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Machine from "./components/Machine";
+
+function App() {
+  return (
+    <>
+      <Machine />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Machine.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Machine = () => {
+  return <div className="probando">Machine</div>;
+};
+
+export default Machine;
